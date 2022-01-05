cb67c56471f26b944073a4a84398600c3ab08c5c
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-05 12:06:15 +0200

    Initial Commit

---
+++ src/test/java/zw/co/afrosoft/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
